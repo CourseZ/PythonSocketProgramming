b8891b16edf30ba70a4497435c8869c7e39c9029
CourseZ <nguyenvanlong17052002@gmail.com>
2023-03-07 21:31:41 +0700

    Version 1.00 Intro socket programming

---
+++ SocketProgrammongGFG/findip.py
@@ -0,0 +1,5 @@
+import socket
+ip = socket.gethostbyname('www.google.com')  # dùng để tìm địa chỉ ip của server
+print(ip)
+
+# output: 172.217.24.100
---
+++ SocketProgrammongGFG/socketprogramming.py
@@ -0,0 +1,4 @@
+import socket
+s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+# AF_INET đề cập đến họ địa chỉ IPv4
+# SOCK_STREAM có nghĩa là giao thức hướng kết nối TCP
